411aa8fdd91ae30ec0ee25899e25fcea5a0d6440
Jade <1621396712@qq.com>
2019-04-04 05:46:15 +0800

    bus

---
+++ src/components/AShow.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Ashow: {{ content }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        content: {
+            type: [String, Number],
+            default: ''
+        }
+    }
+}
+</script>
---
+++ src/views/store.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <a-input @input="handleInput"/>
+        <p>{{ value }}</p>
+        <a-show :content="value"></a-show>
+    </div>
+</template>
+
+<script>
+import AInput from '_c/AInput.vue'
+import AShow from '_c/AShow.vue'
+
+export default {
+    components: {
+        AInput,
+        AShow
+    },
+    data () {
+        return {
+            value: ''
+        }
+    },
+    methods: {
+        handleInput (val) {
+            this.value = val;
+        }
+    }
+}
+</script>
